ef7e1e66ca2981879383862fbd086a661e9d82d4
Deepika Raj <deepika.raj.93@gmail.com>
2017-03-30 10:25:39 +0530

    Added G711 codec

---
+++ per_g711client.c
@@ -0,0 +1,186 @@
+/*
+Periodic Scheduling at client side 
+Bandwidth = Datarate / Sampling rate
+Encoding using g711
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include<signal.h>
+#include <arpa/inet.h>
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+#include<sys/types.h>
+#include "g711.c"
+#include<sys/stat.h>
+#include<fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <pulse/simple.h>
+#include <pulse/error.h>
+#include <pulse/gccmacro.h>
+#define BUFSIZE 1024
+int sockfd,l;
+uint8_t buf[BUFSIZE];
+uint8_t buf2[BUFSIZE];
+volatile sig_atomic_t keep_going = 1;
+static const pa_sample_spec ss = {
+        				.format = PA_SAMPLE_S16LE,
+        				.rate = 44100,
+        				.channels = 2
+   					 };
+	pa_simple *s1 = NULL;
+	pa_simple *s2=NULL;
+	int ret = 1;
+	int error;
+
+/* get sockaddr, IPv4 or IPv6: */
+void *get_in_addr(struct sockaddr *sa)
+{
+	if (sa->sa_family == AF_INET) 
+	{
+		return &(((struct sockaddr_in*)sa)->sin_addr);
+	}	
+
+		return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+void periodic(int signum)
+{
+
+		/* Record some data ... */
+		if (pa_simple_read(s1, buf, sizeof(buf), &error) < 0)
+		{
+			    fprintf(stderr, __FILE__": pa_simple_read() failed: %s\n", pa_strerror(error));
+			    if (s1)
+				pa_simple_free(s1);
+				exit(1);
+		}
+
+		//Encode after reading from microphone
+		for(int i=0;i<BUFSIZE;i++)
+		{
+			buf2[i]=linear2alaw(buf[i]);
+			//printf("Decoding");
+	
+		}
+
+		//Send after encoding
+		//printf("trans");
+		if (send(sockfd, buf2, sizeof(buf), 0) == -1)
+		{ 
+			perror("send");
+			if (s1)
+				pa_simple_free(s1);
+				exit(1);
+		}
+
+}
+
+
+int main(int argc, char *argv[])
+{
+	  
+	char str[80];
+	struct addrinfo hints, *servinfo, *p;
+	int rv;
+	int ch;
+	char s[INET6_ADDRSTRLEN];
+	int fd;
+
+    	/* Create the recording stream */
+	if (!(s1 = pa_simple_new(NULL, argv[0], PA_STREAM_RECORD, NULL, "record", &ss, NULL, NULL, &error)))
+	{
+        	fprintf(stderr, __FILE__": pa_simple_new() failed: %s\n", pa_strerror(error));
+        	goto finish;
+    	}
+	printf("creating recording stream\n");
+        uint8_t buf[BUFSIZE];
+
+
+	if (argc != 3)
+	{	/*check for format of arguments*/
+        	fprintf(stderr,"usage: client hostname\n");
+        	exit(1);
+   	}
+
+ 	memset(&hints, 0, sizeof hints);//fills the struct with 0
+	hints.ai_family = AF_UNSPEC;//v4 or v6
+	hints.ai_socktype = SOCK_STREAM;//stream as opposed to datagram
+
+	if ((rv = getaddrinfo(argv[1], argv[2], &hints, &servinfo)) != 0)
+	{//given a host and a service returns a struct of address info
+        	fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+        	return 1;
+    	}
+
+
+    	/*loop through all the results and connect to the first we can */
+	for(p = servinfo; p != NULL; p = p->ai_next)
+	{
+       		if ((sockfd = socket(p->ai_family, p->ai_socktype,p->ai_protocol)) == -1)
+		{
+           		 perror("client: socket");
+           		 continue;
+       		}
+
+        if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1)
+	{
+		close(sockfd);
+           	perror("client: connect");
+            	continue;
+        }
+
+        break;
+    }
+
+	if (p == NULL) 
+	{
+        	fprintf(stderr, "client: failed to connect\n");
+        	return 2;
+    	}	
+
+	ssize_t r;
+	inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr),s, sizeof s);//convert ipv4 and v6 addresses from binary to text,af,src,dst,size
+	printf("client: connecting to %s\n", s);
+	freeaddrinfo(servinfo); // all done with this structure
+
+	//Periodic
+	
+	struct sigaction sa;
+	struct itimerval timer;
+ 	/* Install periodic_task  as the signal handler for SIGVTALRM. */
+ 	memset (&sa, 0, sizeof (sa));
+ 	sa.sa_handler = &periodic ;
+ 	sigaction (SIGVTALRM, &sa, NULL);
+
+ 	/* Configure the timer to expire after 250m sec... */
+ 	timer.it_value.tv_sec = 0;
+ 	timer.it_value.tv_usec = 1;
+
+ 	/* ... and every 250m sec after that. */
+ 	timer.it_interval.tv_sec = 0;
+ 	timer.it_interval.tv_usec = 1;
+
+ 	/* Start a virtual timer. It counts down whenever this process is    executing. */
+ 	setitimer (ITIMER_VIRTUAL, &timer, NULL);
+
+	if (signal(SIGINT, periodic) == SIG_ERR)//register signal handler
+	printf("\ncan't catch SIGINT\n");
+	
+	  
+	while(1);
+	finish:
+		if (s1)
+		pa_simple_free(s1);
+	return 0;
+}
---
+++ per_g711server.c
@@ -0,0 +1,235 @@
+/* IP phone server side 
+Bandwidth = Data Rate/Sampling rate
+Decoding using g711
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>/* IP phone with periodic scheduling server side */
+#include "g711.c"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <signal.h>
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <pulse/simple.h>
+#include <pulse/error.h>
+#include <pulse/gccmacro.h>
+#define BUFSIZE 1024
+uint8_t buf[BUFSIZE];
+uint8_t buf2[BUFSIZE];
+volatile sig_atomic_t keep_going = 1;
+int numbytes;
+int sockfd, new_fd;  // listen on sock_fd, new connection on new_fd
+static const pa_sample_spec ss = {
+     					 .format = PA_SAMPLE_S16LE,
+        				 .rate = 44100,
+        				 .channels = 2
+   					 };
+pa_simple *s1 = NULL;
+int ret = 1;
+int error;
+int fd;
+#define MAXDATASIZE 1024 // max number of bytes we can get at once 
+#define BACKLOG 10     // how many pending connections queue will hold
+
+/*handler for CTRL+C SIGINT */
+void my_handler_for_sigint(int signumber)
+{
+	char ans[2];
+	if (signumber == SIGINT)
+	{
+		printf("received SIGINT\n");
+		printf("Program received a CTRL-C\n");
+		printf("Terminate Y/N : "); 
+		scanf("%s", ans);
+		if (strcmp(ans,"Y") == 0)
+		{
+			printf("Exiting ....Press any key\n");
+			keep_going = 0;
+
+		}
+	else
+	{
+		printf("Continung ..\n");
+	}
+	}
+}
+
+// get sockaddr, IPv4 or IPv6:
+void *get_in_addr(struct sockaddr *sa)
+{
+	if (sa->sa_family == AF_INET)
+	{
+        	return &(((struct sockaddr_in*)sa)->sin_addr);
+    	}
+	return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+void periodic(int signum)
+{
+	//printf("recie");
+	if ((numbytes = recv(new_fd, buf, BUFSIZE, 0)) == -1)
+	{
+		perror("recv");
+		exit(1);
+	}
+
+	//Decode after receiving
+	for(int i=0;i<BUFSIZE;i++)
+	{
+	buf2[i]=alaw2linear(buf[i]);
+	//printf("Decoding");
+	}
+	
+	//Play after decoding	  
+	/* ... and play it */
+	if (pa_simple_write(s1, buf2, sizeof(buf), &error) < 0)
+	{
+		//printf("Playing");
+		fprintf(stderr, __FILE__": pa_simple_write() failed: %s\n", pa_strerror(error));
+		if (s1)
+        		pa_simple_free(s1);
+	}
+
+}
+int main(int argc,char* argv[])
+{	
+	int rv;
+	struct addrinfo hints, *servinfo, *p;
+	struct sockaddr_storage their_addr; // connector's address information
+	socklen_t sin_size;
+	struct sigaction sa;
+	int yes=1;
+	char s[INET6_ADDRSTRLEN];	
+	if (argc != 2)
+	{
+        	fprintf(stderr,"usage:port number\n");
+        	exit(1);
+   	}
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE; // use my IP
+	if ((rv = getaddrinfo(NULL, argv[1], &hints, &servinfo)) != 0) 
+	{
+       		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+        	return 1;
+    	}	
+
+    // loop through all the results and bind to the first we can
+	for(p = servinfo; p != NULL; p = p->ai_next)
+	{
+        	if ((sockfd = socket(p->ai_family, p->ai_socktype,p->ai_protocol)) == -1) 
+		{
+			perror("server: socket");
+			continue;
+       		}
+
+		if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1)
+		{
+           		perror("setsockopt");
+            		exit(1);
+       		}
+
+        	if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) 
+		{//bind a name to the socket;has no address assigned to it
+           		close(sockfd);
+           		perror("server: bind");
+            		continue;
+       		}
+
+       		break;
+	}	
+	freeaddrinfo(servinfo); // all done with this structure
+	if (p == NULL) 
+	{
+        	fprintf(stderr, "server: failed to bind\n");
+        	exit(1);
+    	}
+	if (listen(sockfd, BACKLOG) == -1) 
+	{//listen for connections
+        	perror("listen");
+        	exit(1);
+   	}
+
+
+	printf("server: waiting for connections...\n"); 
+	if (!(s1 = pa_simple_new(NULL, argv[0], PA_STREAM_PLAYBACK, NULL, "playback", &ss, NULL, NULL, &error))) 
+	{
+        	fprintf(stderr, __FILE__": pa_simple_new() failed: %s\n", pa_strerror(error));
+        	goto finish;
+    	}
+
+	//uint8_t buf[BUFSIZE];
+        //ssize_t r;
+
+	while(1)
+        {
+		sin_size = sizeof their_addr;
+        	new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);//accept a connection
+	        if (new_fd == -1)
+		{
+        		perror("accept");
+	        }
+
+		inet_ntop(their_addr.ss_family,
+		get_in_addr((struct sockaddr *)&their_addr),s, sizeof s);
+		printf("server: got connection from %s\n", s);
+		
+		//Periodic Scheduling
+		
+		struct itimerval timer;
+
+	 	/* Install periodic_task  as the signal handler for SIGVTALRM. */
+	 	memset (&sa, 0, sizeof (sa));
+	 	sa.sa_handler = &periodic ;
+	 	sigaction (SIGVTALRM, &sa, NULL);
+
+	 	/* Configure the timer to expire after 250m sec... */
+	 	timer.it_value.tv_sec = 0;
+	 	timer.it_value.tv_usec = 1;
+
+	 	/* ... and every 250m sec after that. */
+	 	timer.it_interval.tv_sec = 0;
+	 	timer.it_interval.tv_usec = 1;
+
+	 	/* Start a virtual timer. It counts down whenever this process is    executing. */
+	 	setitimer (ITIMER_VIRTUAL, &timer, NULL);
+		while(1);
+
+	}
+	//close(new_fd); 
+	
+	if (pa_simple_drain(s1, &error) < 0)
+	{
+        	fprintf(stderr, __FILE__": pa_simple_drain() failed: %s\n", pa_strerror(error));
+        	goto finish;
+    	}
+	ret = 0;
+	finish:
+		if (s1)
+        	pa_simple_free(s1);
+
+	return ret;
+	return 0;
+}
+
